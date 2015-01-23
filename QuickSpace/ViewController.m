024028e3345064a55c67bc901a99f644754a0547
goetomo <gene.oetomo@gmail.com>
2015-01-23 04:19:56 -0800

    Revert "update storyboard"
    This reverts commit 93b76670338e0cbf825cbf5d809b96f4559a6be5.
    
    Conflicts:
    
    QuickSpace.xcodeproj/project.xcworkspace/xcuserdata/geneoetomo.xcuserdat
    ad/UserInterfaceState.xcuserstate

--- QuickSpace/ViewController.m
+++ QuickSpace/ViewController.m
@@ -15,14 +15,9 @@
 
 @implementation ViewController
 
-NSArray * listings;
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    listings = [NSArray arrayWithObjects:@"Gene's Room", @"Tony's Room", "Jordan's Room", nil];
-    
-    
     
     // Just a quick test to make sure that parse is working
     PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
@@ -33,26 +28,6 @@ NSArray * listings;
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [listings count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *simpleTableIdentifier = @"ListingCell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-    }
-    UILabel * title = (UILabel *)[cell viewWithTag:1];
-    title.text = [listings objectAtIndex:indexPath.row];
-//    cell.textLabel.text = [listings objectAtIndex:indexPath.row];
-    return cell;
 }
 
 @end
