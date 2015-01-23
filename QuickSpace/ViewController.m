046f6070e98dd684382a0413989b0ce4c33fb382
goetomo <gene.oetomo@gmail.com>
2015-01-23 04:21:17 -0800

    Revert 93b7667..5b96696
    This rolls back to commit 93b76670338e0cbf825cbf5d809b96f4559a6be5.

--- QuickSpace/ViewController.m
+++ QuickSpace/ViewController.m
@@ -15,9 +15,14 @@
 
 @implementation ViewController
 
+NSArray * listings;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    listings = [NSArray arrayWithObjects:@"Gene's Room", @"Tony's Room", "Jordan's Room", nil];
+    
+    
     
     // Just a quick test to make sure that parse is working
     PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
@@ -28,6 +33,26 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [listings count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"ListingCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    UILabel * title = (UILabel *)[cell viewWithTag:1];
+    title.text = [listings objectAtIndex:indexPath.row];
+//    cell.textLabel.text = [listings objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
