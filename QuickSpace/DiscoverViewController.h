21bb69ab6956e9256d3f6a2237e04b1a371e8fbd
Tony Wang <wang8@myth22.Stanford.EDU>
2015-01-25 16:41:23 -0800

    updated storyboard with discover

---
+++ QuickSpace/DiscoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoverViewController.h
+//  QuickSpace
+//
+//  Created by Tony Wang on 1/25/15.
+//  Copyright (c) 2015 Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscoverViewController : UIViewController
+
+@end
