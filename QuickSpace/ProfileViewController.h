396a89c8acc61f89485f4e2a5ab8be94a90783fe
Tony Wang <wang.xinlei15@gmail.com>
2015-01-31 16:16:55 -0800

    added profile view controller

---
+++ QuickSpace/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  QuickSpace
+//
+//  Created by Tony Wang on 1/31/15.
+//  Copyright (c) 2015 Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
