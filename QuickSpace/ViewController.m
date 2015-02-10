45ce455df1e1e3712d194697d016ca08cda9c325
Jordan <jordans@stanford.edu>
2015-02-10 14:37:33 -0800

    fuck

--- QuickSpace/ViewController.m
+++ QuickSpace/ViewController.m
@@ -76,7 +76,7 @@ NSArray *listings;
 //    [self.locationManager stopUpdatingLocation];
     
     PFQuery *fakeQuery = [PFQuery queryWithClassName:@"Listing"];
-    [fakeQuery whereKey:@"location" nearGeoPoint:discoverLocation withinKilometers:50];
+    [fakeQuery whereKey:@"location" nearGeoPoint:discoverLocation];
 
     
     listings = [Listing objectToListingsWith:[fakeQuery findObjects]];
